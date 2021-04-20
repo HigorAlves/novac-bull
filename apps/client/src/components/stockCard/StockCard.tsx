1bc54df37bbc4070ac551a08247898530d1248de
Higor Alves <higorhaalves@gmail.com>
2021-04-20 14:25:07 -0300

    chore(client): stock cards creation

--- apps/client/src/components/stockCard/StockCard.tsx
+++ apps/client/src/components/stockCard/StockCard.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 
-import { Paper } from '@material-ui/core'
+import { Button, Paper, Typography } from '@material-ui/core'
 
-// TODO: Colocar a foto da ação
-// TODO: Colocar o nome com simbolo
-// TODO: Colocar  o preço da ação
+import { numberToMoney } from 'utils/numberToMoney'
+
 export function StockCard() {
 	return (
-		<Paper elevation={0}>
-			<h1>asdf</h1>
-		</Paper>
+		<>
+			<Paper elevation={0} style={{ textAlign: 'center' }}>
+				<Typography variant={'h5'} component={'h6'}>
+					Apple Inc.
+				</Typography>
+				<Typography variant={'body2'} component={'p'}>
+					AAPL
+				</Typography>
+				<Typography variant={'h5'} component={'h6'}>
+					{numberToMoney(172.32, 'pt-BR', 'BRL')}
+				</Typography>
+			</Paper>
+			<Button variant={'contained'} color={'primary'} fullWidth>
+				COMPRAR
+			</Button>
+		</>
 	)
 }
