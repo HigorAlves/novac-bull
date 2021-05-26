44f8f00aea284d57876f18a43e3883fa1728a47b
Higor Alves <higorhaalves@gmail.com>
2021-05-26 11:49:56 -0300

    fix: upgrade webpack 5
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- apps/ssr/next.config.js
+++ apps/ssr/next.config.js
@@ -1,28 +1,31 @@
 const withPlugins = require('next-compose-plugins')
 
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-    return config
-  },
-  trailingSlash: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY'
-          }
-        ]
-      }
-    ]
-  }
+	future: {
+		webpack5: true
+	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.node = {
+				fs: 'empty'
+			}
+		}
+		return config
+	},
+	trailingSlash: true,
+	async headers() {
+		return [
+			{
+				source: '/:path*',
+				headers: [
+					{
+						key: 'X-Frame-Options',
+						value: 'DENY'
+					}
+				]
+			}
+		]
+	}
 }
 
 module.exports = withPlugins([], nextConfig)
