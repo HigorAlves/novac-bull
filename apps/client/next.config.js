d367af296fc2f6af6d1823708d4640f1dce04a71
Higor Alves <higorhaalves@gmail.com>
2021-06-08 22:18:06 -0300

    refactor: removed babelrc
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
-  productionBrowserSourceMaps: true,
-  trailingSlash: true
+	future: {
+		webpack5: true
+	},
+	productionBrowserSourceMaps: true,
+	trailingSlash: true
 }
