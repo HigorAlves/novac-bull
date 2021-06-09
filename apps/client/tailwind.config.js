af7128f29abc4090727197a5d888e7e84f89f9ee
Cleber Henrique Soares <cleberschiavon@outlook.com>
2021-06-08 23:12:42 -0300

    feat(client): adding tailwind grid

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,7 +1,16 @@
+const {breakpoints} = require('./src/assets/styles/grid');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: `${breakpoints.sm}px`,
+      md: `${breakpoints.md}px`,
+      lg: `${breakpoints.lg}px`,
+      xl: `${breakpoints.xl}px`,
+      xxl: `${breakpoints.xxl}px`
+    },
     extend: {},
   },
   variants: {
