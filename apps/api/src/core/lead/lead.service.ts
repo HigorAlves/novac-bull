c7cb2a4faf67e9f6a536108f9174a7cc8f7a36a0
Higor Alves <higorhaalves@gmail.com>
2021-06-08 23:46:48 -0300

    refactor(api): lead error handling with sentry
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- apps/api/src/core/lead/lead.service.ts
+++ apps/api/src/core/lead/lead.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+import * as Sentry from '@sentry/node'
 
 import { HTTP_CODE } from '~/constants/httpCode'
 import { LeadRepository } from '~/core/lead/lead.repository'
@@ -11,21 +12,31 @@ export class LeadService {
 	}
 
 	async registerNewLead(email: string): Promise<IResponse> {
-		const result = await this.repository.create(email)
-		if (result) {
-			this.logger.log('New lead was registered')
+		try {
+			const result = await this.repository.create(email)
+			if (result) {
+				this.logger.log('New lead was registered')
 
-			return {
-				status: HTTP_CODE.Created,
-				error: false,
-				message: 'Lead has been registrated'
+				return {
+					status: HTTP_CODE.Created,
+					error: false,
+					message: 'Lead has been registrated'
+				}
 			}
-		}
-
-		return {
-			status: HTTP_CODE.BadRequest,
-			error: true,
-			message: 'Something was wrong'
+		} catch (e) {
+			Sentry.withScope(scope => {
+				scope.addBreadcrumb({
+					category: 'LEAD',
+					message: 'Cannont register a new lead inside DB',
+					level: Sentry.Severity.Warning
+				})
+				Sentry.captureException(new Error(e))
+			})
+			return {
+				status: HTTP_CODE.BadRequest,
+				error: true,
+				message: 'Something was wrong'
+			}
 		}
 	}
 }
