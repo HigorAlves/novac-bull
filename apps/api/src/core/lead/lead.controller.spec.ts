4361b697fc3ecd23c1ab6f3f84c5a2beeb568a61
Higor Alves <higorhaalves@gmail.com>
2021-06-09 10:37:28 -0300

    test(api): new actions for CI
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- apps/api/src/core/lead/lead.controller.spec.ts
+++ apps/api/src/core/lead/lead.controller.spec.ts
@@ -1,12 +1,24 @@
+import { MongooseModule } from '@nestjs/mongoose'
 import { Test, TestingModule } from '@nestjs/testing'
 
+import { rootMongooseTestModule } from '../../../test/utils/mongodb'
 import { LeadController } from './lead.controller'
+import { LeadRepository } from '~/core/lead/lead.repository'
+import { LeadService } from '~/core/lead/lead.service'
+import { LoggerModule } from '~/interceptors/logger.interceptor'
+import { LeadSchema } from '~/schemas/lead.schema'
 
 describe('LeadController', () => {
 	let controller: LeadController
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
+			imports: [
+				rootMongooseTestModule(),
+				MongooseModule.forFeature([{ name: 'Lead', schema: LeadSchema }]),
+				LoggerModule
+			],
+			providers: [LeadService, LeadRepository],
 			controllers: [LeadController]
 		}).compile()
 
