26a8377fb7f785589af436f7bb355ed9352b427d
Cleber Henrique Soares <cleberschiavon@outlook.com>
2021-06-08 23:14:40 -0300

    feat(client): adding tailwind style utilities

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,9 +1,12 @@
 const {breakpoints} = require('./src/assets/styles/grid');
+const { spacing, borderRadius, borderWidth, borderColor } = require('./src/assets/styles/tailwind/utilities');
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    borderWidth,
+    borderColor,
     screens: {
       sm: `${breakpoints.sm}px`,
       md: `${breakpoints.md}px`,
@@ -11,6 +14,8 @@ module.exports = {
       xl: `${breakpoints.xl}px`,
       xxl: `${breakpoints.xxl}px`
     },
+    spacing,
+    borderRadius,
     extend: {},
   },
   variants: {
