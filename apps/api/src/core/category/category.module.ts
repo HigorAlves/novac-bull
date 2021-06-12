8c0e8f5e9e126a950c48265ad177e9261ff95ae1
Higor Alves <higorhaalves@gmail.com>
2021-06-11 21:55:11 -0300

    test(api): category test files
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- apps/api/src/core/category/category.module.ts
+++ apps/api/src/core/category/category.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 
 import { CategoryService } from './category.service'
+import { UserModule } from '~/core/user/user.module'
+import { LoggerModule } from '~/interceptors/logger.interceptor'
+import { UserSchema } from '~/schemas/user.schema'
 
 @Module({
+	imports: [
+		LoggerModule,
+		UserModule,
+		MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
+	],
 	providers: [CategoryService]
 })
-export class CategoryModule {}
+export class CategoryModule {
+}
--- apps/api/src/core/category/category.service.spec.ts
+++ apps/api/src/core/category/category.service.spec.ts
@@ -1,16 +1,32 @@
+import { MongooseModule } from '@nestjs/mongoose'
 import { Test, TestingModule } from '@nestjs/testing'
 
+import { closeInMongodConnection, rootMongooseTestModule } from '../../../test/utils/mongodb'
 import { CategoryService } from './category.service'
+import { UserModule } from '~/core/user/user.module'
+import { LoggerModule } from '~/interceptors/logger.interceptor'
+import { UserSchema } from '~/schemas/user.schema'
 
-describe('CategoryService', () => {
+describe('Category Service', () => {
 	let service: CategoryService
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
+			imports: [
+				rootMongooseTestModule(),
+				LoggerModule,
+				MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+				UserModule
+			],
 			providers: [CategoryService]
 		}).compile()
 
 		service = module.get<CategoryService>(CategoryService)
+	})
+
+	afterAll(done => {
+		closeInMongodConnection()
+		done()
 	})
 
 	it('should be defined', () => {
