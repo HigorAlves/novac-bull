6c008d4ee3e66675cd367180ed912bf97f5e7959
Higor Alves <higorhaalves@gmail.com>
2021-06-12 19:13:42 -0300

    feat(api): create category service
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- apps/api/src/core/category/category.service.ts
+++ apps/api/src/core/category/category.service.ts
@@ -1,4 +1,34 @@
+import { ICategory, IResponse } from '@jetpack/interfaces'
 import { Injectable } from '@nestjs/common'
 
+import { HTTP_CODE } from '~/constants/httpCode'
+import { CategoryRepository } from '~/core/category/category.repository'
+import { Logger } from '~/interceptors/logger.interceptor'
+
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+	constructor(
+		private readonly repository: CategoryRepository,
+		private readonly logger: Logger
+	) {
+		this.logger.setContext('CATEGORY_SERVICE')
+	}
+
+	async create(userId: string, category: ICategory): Promise<IResponse> {
+		try {
+			await this.repository.create(userId, category)
+			return {
+				error: false,
+				message: 'New category added',
+				status: 201
+			}
+		} catch (e) {
+			this.logger.error(e)
+			return {
+				error: true,
+				message: e.message,
+				status: HTTP_CODE.BadRequest
+			}
+		}
+	}
+}
