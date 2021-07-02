816d480ea3a6e3c3d5a8466487629671d15bb15a
Cleber Henrique Soares <cleberschiavon@outlook.com>
2021-07-01 22:28:17 -0300

    feat(client): adding button component

---
+++ apps/client/src/components/buttons/styled.button.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+interface IBaseComponent {
+	primary: boolean
+	secondary: boolean
+}
+
+export const BaseComponent = styled.button(
+	({ primary, secondary }: IBaseComponent) => [
+		tw`ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-md shadow-sm text-base font-medium`,
+		primary && tw`text-white bg-indigo-600 hover:bg-indigo-700`
+	]
+)
