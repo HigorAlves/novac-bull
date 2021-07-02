aaf6c676833f1cdbb894943a646c76b67f3c1c68
Cleber Henrique Soares <cleberschiavon@outlook.com>
2021-07-01 23:37:20 -0300

    feat(client): secondary color in button

--- apps/client/src/components/buttons/styled.button.ts
+++ apps/client/src/components/buttons/styled.button.ts
@@ -9,6 +9,7 @@ interface IBaseComponent {
 export const BaseComponent = styled.button(
 	({ primary, secondary }: IBaseComponent) => [
 		tw`ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-md shadow-sm text-base font-medium`,
-		primary && tw`text-white bg-indigo-600 hover:bg-indigo-700`
+		primary && tw`text-white bg-indigo-600 hover:bg-indigo-700`,
+		secondary && tw`text-white bg-purple-600 hover:bg-purple-600`
 	]
 )
