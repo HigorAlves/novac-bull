b1bb55f2f9c616f891644cb374f71a7c3e2714e0
Cleber Henrique Soares <cleberschiavon@outlook.com>
2021-07-02 00:43:00 -0300

    chore(client): Adding Responsive Margin in Button

--- apps/client/src/components/buttons/styled.button.ts
+++ apps/client/src/components/buttons/styled.button.ts
@@ -8,7 +8,7 @@ interface IBaseComponent {
 
 export const BaseComponent = styled.button(
 	({ primary, secondary }: IBaseComponent) => [
-		tw`ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-md shadow-sm text-base font-medium`,
+		tw`sm:ml-8 ml-0 whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-md shadow-sm text-base font-medium`,
 		primary && tw`text-white bg-indigo-600 hover:bg-indigo-700`,
 		secondary && tw`text-white bg-purple-600 hover:bg-purple-600`
 	]
