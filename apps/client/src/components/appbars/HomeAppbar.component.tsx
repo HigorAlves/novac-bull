592008d3d5f209c423832ad9b1fab068af466e1d
Cleber Henrique Soares <cleberschiavon@outlook.com>
2021-07-05 01:40:59 -0300

    feat(client): ancoring sections in AppBar

--- apps/client/src/components/appbars/HomeAppbar.component.tsx
+++ apps/client/src/components/appbars/HomeAppbar.component.tsx
@@ -52,16 +52,16 @@ export default function HomeAppbar() {
 								</div>
 								<Popover.Group as='nav' className='hidden md:flex space-x-10'>
 									<a
-										href='#'
+										href='#pre-register'
 										className='text-base font-medium text-gray-500 hover:text-gray-900'
 									>
 										Pré-Registro
 									</a>
 									<a
-										href='#'
+										href='#whynovac'
 										className='text-base font-medium text-gray-500 hover:text-gray-900'
 									>
-										Sobre Nós
+										Por quê o Novac?
 									</a>
 
 									<Popover className='relative'>
@@ -124,7 +124,7 @@ export default function HomeAppbar() {
 								<div className='hidden md:flex items-center justify-end md:flex-1 lg:w-0'>
 									<a
 										href='#'
-										className='whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900'
+										className='whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 mr-4'
 									>
 										Entrar
 									</a>
@@ -187,10 +187,10 @@ export default function HomeAppbar() {
 											</a>
 
 											<a
-												href='#'
+												href='#whynovac'
 												className='text-base font-medium text-gray-900 hover:text-gray-700'
 											>
-												Sobre Nós
+												Por quê o Novac?
 											</a>
 										</div>
 										<div>
